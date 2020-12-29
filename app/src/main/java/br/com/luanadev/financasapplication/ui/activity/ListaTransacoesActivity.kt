d5ad0cd1bb2fcfbfcaf16cb062fc8455463f3f10
Luana Souza <luana.souza@ifood.com.br>
2020-12-29 17:09:28 -0300

    implementando DtaePickerDialog

--- app/src/main/java/br/com/luanadev/financasapplication/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/luanadev/financasapplication/ui/activity/ListaTransacoesActivity.kt
@@ -1,23 +1,60 @@
 package br.com.luanadev.financasapplication.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import br.com.luanadev.financasapplication.R
+import br.com.luanadev.financasapplication.extension.formataDataParaBrasileiro
 import br.com.luanadev.financasapplication.model.Tipo
 import br.com.luanadev.financasapplication.model.Transacao
 import br.com.luanadev.financasapplication.ui.ResumoView
 import br.com.luanadev.financasapplication.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.util.*
 
-class   ListaTransacoesActivity : AppCompatActivity() {
+class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
         val transacoes: List<Transacao> = transacoesDeExemplo()
         configuraResumo(transacoes)
         configuraLista(transacoes)
+
+        lista_transacoes_adiciona_receita.setOnClickListener {
+            val view: View = window.decorView
+            val viewCriada = LayoutInflater.from(this)
+                .inflate(R.layout.form_transacao, view as ViewGroup, false)
+
+            val ano = 2020
+            val mes = 11
+            val dia = 29
+            val hoje = Calendar.getInstance()
+            viewCriada.form_transacao_data.setText(hoje.formataDataParaBrasileiro())
+            viewCriada.form_transacao_data.setOnClickListener{
+                DatePickerDialog(this,
+                    { view, ano, mes, dia ->
+                        val dataSelecionada = Calendar.getInstance()
+                        dataSelecionada.set(ano, mes, dia)
+                        viewCriada.form_transacao_data.setText(dataSelecionada.formataDataParaBrasileiro())
+                    }, ano, mes, dia).show()
+            }
+
+            AlertDialog.Builder(this)
+                .setTitle(R.string.adiciona_receita)
+                .setView(viewCriada)
+                .show()
+        }
     }
 
     private fun configuraResumo(transacoes: List<Transacao>) {
@@ -26,17 +63,28 @@ class   ListaTransacoesActivity : AppCompatActivity() {
         resumoView.atualiza()
     }
 
-
     private fun configuraLista(transacoes: List<Transacao>) {
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 
     private fun transacoesDeExemplo() = listOf(
-        Transacao(valor = BigDecimal(20.5), tipo = Tipo.DESPESA, categoria = "Almoço de Final de Semana"),
+        Transacao(
+            valor = BigDecimal(20.5),
+            tipo = Tipo.DESPESA,
+            categoria = "Almoço de Final de Semana"
+        ),
         Transacao(valor = BigDecimal(100.0), tipo = Tipo.RECEITA, categoria = "Economia"),
-        Transacao(valor = BigDecimal(20.5), tipo = Tipo.DESPESA, categoria = "Almoço de Final de Semana"),
+        Transacao(
+            valor = BigDecimal(20.5),
+            tipo = Tipo.DESPESA,
+            categoria = "Almoço de Final de Semana"
+        ),
         Transacao(valor = BigDecimal(100.0), tipo = Tipo.RECEITA, categoria = "Economia"),
-        Transacao(valor = BigDecimal(20.5), tipo = Tipo.DESPESA, categoria = "Almoço de Final de Semana"),
+        Transacao(
+            valor = BigDecimal(20.5),
+            tipo = Tipo.DESPESA,
+            categoria = "Almoço de Final de Semana"
+        ),
         Transacao(valor = BigDecimal(100.0), tipo = Tipo.RECEITA, categoria = "Economia")
     )
 }
